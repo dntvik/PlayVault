634e6117900975346aa3a17bb000d2012f3bbd8e
dntvik <viktor.ganshin@gmail.com>
2024-11-10 07:05:43 +0200

    fix_tests

--- src/games/views.py
+++ src/games/views.py
@@ -12,7 +12,7 @@ class GameListView(ListView):
 
 class GenreDetailView(ListView):
     model = Game
-    template_name = "genres_list.html"  # Создайте этот шаблон
+    template_name = "genres_list.html"
     context_object_name = "games"
 
     def get_queryset(self):
