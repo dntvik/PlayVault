d350d8afd6b2bb556a75881484d4058f422460db
dntvik <viktor.ganshin@gmail.com>
2024-11-10 22:15:37 +0200

    added_some_html

--- src/config/urls.py
+++ src/config/urls.py
@@ -9,22 +9,22 @@ from common.views import IndexView
 from games.views import GenreDetailView, PlatformDetailView
 
 urlpatterns = (
-        [
-            path("", IndexView.as_view(), name="index"),
-            path("registration/", UserRegistration.as_view(), name="registration"),
-            path("login/", UserLogin.as_view(), name="login"),
-            path("logout/", UserLogout.as_view(), name="logout"),
-            path("profile/", UserProfileView.as_view(), name="user_profile"),
-            path("oauth/", include("social_django.urls", namespace="social")),
-            path("admin/", admin.site.urls),
-            path("games/", include("games.urls")),
-            path("genres/<int:id>/", GenreDetailView.as_view(), name="genres_list"),
-            path("platforms/<int:id>/", PlatformDetailView.as_view(), name="platforms_list"),
-            path("api-auth/", include("rest_framework.urls")),
-            path("api/", include("api.urls")),
-            path("blog/", include("blog.urls")),
-        ]
-        + debug_toolbar_urls()  # NOQA W503
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # NOQA W503
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # NOQA W503
+    [
+        path("", IndexView.as_view(), name="index"),
+        path("registration/", UserRegistration.as_view(), name="registration"),
+        path("login/", UserLogin.as_view(), name="login"),
+        path("logout/", UserLogout.as_view(), name="logout"),
+        path("profile/", UserProfileView.as_view(), name="user_profile"),
+        path("oauth/", include("social_django.urls", namespace="social")),
+        path("admin/", admin.site.urls),
+        path("games/", include("games.urls")),
+        path("genres/<int:id>/", GenreDetailView.as_view(), name="genres_list"),
+        path("platforms/<int:id>/", PlatformDetailView.as_view(), name="platforms_list"),
+        path("api-auth/", include("rest_framework.urls")),
+        path("api/", include("api.urls")),
+        path("blog/", include("blog.urls")),
+    ]
+    + debug_toolbar_urls()  # NOQA W503
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # NOQA W503
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # NOQA W503
 )
