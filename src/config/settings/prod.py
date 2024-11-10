a3b8ae629ef3285fc133f6400a42c4b2ef03bc31
dntvik <viktor.ganshin@gmail.com>
2024-11-10 07:18:45 +0200

    edit_prod_settings

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -6,7 +6,7 @@ from config.settings.base import *  # NOQA
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
