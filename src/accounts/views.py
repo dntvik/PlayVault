d306f4983fc706e898abdd74f03c9e4ab08218cd
dntvik <viktor.ganshin@gmail.com>
2024-11-25 22:21:29 +0200

    generate_accounts

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 
 from accounts.forms import UserRegistrationForm
 from accounts.models import UserProfile
+from accounts.tasks import generate_accounts
 
 
 def send_registration_email(user_instance, request):
@@ -44,3 +46,10 @@ class UserProfileView(DetailView):
             return UserProfile.objects.get(user=user)
         except UserProfile.DoesNotExist:
             return None
+
+
+def generate_accounts_view(request: HttpRequest) -> HttpResponse:
+    model_name = request.GET.get("model_name", "UserProfile")
+    count = int(request.GET.get("count", 100))
+    generate_accounts.delay(model_name, count)
+    return HttpResponse(f"Task to generate {count} accounts in {model_name} is started.")
